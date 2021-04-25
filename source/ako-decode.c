657585bd5fd55e1bf48539cd1332055f8e76ae8c
Alexander Brandt <brandt.alxndr@outlook.com>
2021-04-25 15:51:19 -0300

    Fixed memory issues and simplified 2d routines

--- source/ako-decode.c
+++ source/ako-decode.c
@@ -80,9 +80,65 @@ static inline void sReadHead(const void* blob, size_t* dimension, size_t* channe
 }
 
 
-static void sDeTransformColorFormat(size_t dimension, size_t channels, const int16_t* input, uint8_t* out)
+static void sDeTransformColorFormat(size_t dimension, size_t channels, int16_t* input, uint8_t* out)
 {
-	int16_t* out16 = (int16_t*)out; // HACK!
+	// Color transformation, YCoCg to sRgb
+	// https://en.wikipedia.org/wiki/YCoCg
+
+#if (AKO_DECODER_COLOR_TRANSFORMATION == 1)
+	DevBenchmarkStart("DeTransform - Color");
+	if (channels == 3)
+	{
+		for (size_t i = 0; i < (dimension * dimension); i++)
+		{
+			const int16_t y = input[(dimension * dimension * 0) + i];
+			const int16_t co = input[(dimension * dimension * 1) + i] - 128;
+			const int16_t cg = input[(dimension * dimension * 2) + i] - 128;
+
+			const int16_t r = (y - cg + co);
+			const int16_t g = (y + cg);
+			const int16_t b = (y - cg - co);
+
+			input[(dimension * dimension * 0) + i] = (r > 0) ? (r < 255) ? r : 255 : 0;
+			input[(dimension * dimension * 1) + i] = (g > 0) ? (g < 255) ? g : 255 : 0;
+			input[(dimension * dimension * 2) + i] = (b > 0) ? (b < 255) ? b : 255 : 0;
+		}
+	}
+	else if (channels == 4)
+	{
+		for (size_t i = 0; i < (dimension * dimension); i++)
+		{
+			const int16_t y = input[(dimension * dimension * 0) + i];
+			const int16_t co = input[(dimension * dimension * 1) + i] - 128;
+			const int16_t cg = input[(dimension * dimension * 2) + i] - 128;
+			const int16_t a = input[(dimension * dimension * 3) + i];
+
+			const int16_t r = (y - cg + co);
+			const int16_t g = (y + cg);
+			const int16_t b = (y - cg - co);
+
+			input[(dimension * dimension * 0) + i] = (r > 0) ? (r < 255) ? r : 255 : 0;
+			input[(dimension * dimension * 1) + i] = (g > 0) ? (g < 255) ? g : 255 : 0;
+			input[(dimension * dimension * 2) + i] = (b > 0) ? (b < 255) ? b : 255 : 0;
+			input[(dimension * dimension * 3) + i] = (a > 0) ? (a < 255) ? a : 255 : 0;
+		}
+	}
+	else
+	{
+		for (size_t i = 0; i < (dimension * dimension * channels); i++)
+		{
+			const int16_t c = input[i];
+			input[i] = (c > 0) ? (c < 255) ? c : 255 : 0;
+		}
+	}
+	DevBenchmarkStop();
+#else
+	for (size_t i = 0; i < (dimension * dimension * channels); i++)
+	{
+		const int16_t c = input[i];
+		input[i] = (c > 0) ? (c < 255) ? c : 255 : 0;
+	}
+#endif
 
 	// Interlace from planes
 	DevBenchmarkStart("DeTransform - Format");
@@ -90,137 +146,76 @@ static void sDeTransformColorFormat(size_t dimension, size_t channels, const int
 	{
 	case 4:
 		for (size_t i = 0; i < (dimension * dimension); i++)
-			out16[i * channels + 3] = input[(dimension * dimension * 3) + i];
+			out[i * channels + 3] = (uint8_t)input[(dimension * dimension * 3) + i];
 	case 3:
 		for (size_t i = 0; i < (dimension * dimension); i++)
-			out16[i * channels + 2] = input[(dimension * dimension * 2) + i];
+			out[i * channels + 2] = (uint8_t)input[(dimension * dimension * 2) + i];
 	case 2:
 		for (size_t i = 0; i < (dimension * dimension); i++)
-			out16[i * channels + 1] = input[(dimension * dimension * 1) + i];
+			out[i * channels + 1] = (uint8_t)input[(dimension * dimension * 1) + i];
 	case 1:
 		for (size_t i = 0; i < (dimension * dimension); i++)
-			out16[i * channels] = input[i];
+			out[i * channels] = (uint8_t)input[i];
 	}
 	DevBenchmarkStop();
-
-#if (AKO_DECODER_COLOR_TRANSFORMATION == 1)
-	// Color transformation, YCoCg to sRgb
-	// https://en.wikipedia.org/wiki/YCoCg
-
-	DevBenchmarkStart("DeTransform - Color");
-	if (channels == 3 || channels == 4)
-	{
-		for (size_t i = 0; i < (dimension * dimension * channels); i += channels)
-		{
-			const int16_t y = out16[i];
-			const int16_t co = (out16[i + 1] - 128);
-			const int16_t cg = (out16[i + 2] - 128);
-
-			const int16_t r = (y - cg + co);
-			const int16_t g = (y + cg);
-			const int16_t b = (y - cg - co);
-
-			out[i] = (r > 0) ? (r < 255) ? (uint8_t)r : 255 : 0;
-			out[i + 1] = (g > 0) ? (g < 255) ? (uint8_t)g : 255 : 0;
-			out[i + 2] = (b > 0) ? (b < 255) ? (uint8_t)b : 255 : 0;
-		}
-	}
-	else
-	{
-		for (size_t i = 0; i < (dimension * dimension * channels); i++)
-			out[i] = (uint8_t)out16[i];
-	}
-	DevBenchmarkStop();
-#else
-	for (size_t i = 0; i < (dimension * dimension * channels); i++)
-		out[i] = (uint8_t)out16[i];
-#endif
 }
 
 
 #if (AKO_DECODER_WAVELET_TRANSFORMATION == 1)
-static void sUnlift1d(size_t len, const int16_t* in, int16_t* out)
+static inline void sUnlift1d(size_t len, const int16_t* in, int16_t* out)
 {
+#if 1
 	// Even
-	for (size_t i = 0; i < len; i++)
+	out[0] = in[0] - (in[len] * 2) / 2;
+
+	for (size_t i = 1; i < len; i++)
 	{
 		const int16_t hp = in[len + i] * 2;          // De-squish
 		const int16_t hp_prev = in[len + i - 1] * 2; // Ditto
-
-		if (i > 0)
-			out[i * 2] = in[i] - (hp + hp_prev) / 4;
-		else
-			out[i * 2] = in[i] - (hp) / 2;
+		out[i << 1] = in[i] - (hp + hp_prev) / 4;
 	}
 
 	// Odd
-	for (size_t i = 0; i < len; i++)
+	for (size_t i = 0; i < (len - 2); i++)
 	{
 		const int16_t hp = in[len + i] * 2;
-
-		if (i < len - 2)
-			out[i * 2 + 1] = hp + (out[i * 2] + out[(i + 1) * 2]) / 2;
-		else
-			out[i * 2 + 1] = hp + (out[i * 2]);
+		out[(i << 1) + 1] = hp + (out[i << 1] + out[(i << 1) + 2]) / 2;
 	}
+
+	out[((len - 2) << 1) + 1] = (in[len + (len - 2)] * 2) + (out[(len - 2) << 1]);
+	out[((len - 1) << 1) + 1] = (in[len + (len - 1)] * 2) + (out[(len - 1) << 1]);
+#endif
 }
 
 static void sUnlift2d(size_t dimension, size_t final_dimension, int16_t* aux_buffer, int16_t* inout)
 {
-	union
-	{
-		int16_t *row, *col;
-	} in;
-	union
-	{
-		int16_t *row, *col;
-	} out;
-
-	int16_t buffer_a[512]; // FIXME, Ahhh!
-	int16_t buffer_b[512]; // FIXME, Aaahhh!!!
-
 	// Columns
 	for (size_t i = 0; i < dimension; i++)
 	{
-		in.col = inout + i;
+		int16_t* temp = inout + i;
 		for (size_t u = 0; u < dimension; u++)
-		{
-			buffer_a[u] = *in.col;
-			in.col = in.col + final_dimension;
-		}
+			aux_buffer[u] = temp[u * final_dimension];
 
-		sUnlift1d(dimension / 2, buffer_a, buffer_b);
+		sUnlift1d(dimension / 2, aux_buffer, aux_buffer + dimension);
 
-		out.col = aux_buffer + i;
+		temp = inout + i;
 		for (size_t u = 0; u < dimension; u++)
-		{
-			*out.col = buffer_b[u];
-			out.col = out.col + final_dimension;
-		}
+			temp[u * final_dimension] = aux_buffer[dimension + u];
 	}
 
 	// Rows
-	in.row = aux_buffer;
-	out.row = inout;
-
 	for (size_t i = 0; i < dimension; i++)
 	{
-		sUnlift1d(dimension / 2, in.row, out.row);
-		in.row = in.row + final_dimension;
-		out.row = out.row + final_dimension;
+		memcpy(aux_buffer, inout, sizeof(int16_t) * dimension);
+		sUnlift1d(dimension / 2, aux_buffer, inout);
+		inout = inout + final_dimension;
 	}
 }
 
-static void sUnliftPlane(size_t dimension, void* aux_buffer, void* inout)
+static inline void sUnliftPlane(size_t dimension, void** aux_buffer, void* inout)
 {
-	size_t current_dimension = 4;
-	size_t final_dimension = dimension;
-
-	while (current_dimension <= final_dimension)
-	{
-		sUnlift2d(current_dimension, final_dimension, aux_buffer, inout);
-		current_dimension = current_dimension * 2;
-	}
+	for (size_t current = 4; current <= dimension; current = current * 2)
+		sUnlift2d(current, dimension, *aux_buffer, inout);
 }
 #endif
 
@@ -233,14 +228,14 @@ uint8_t* AkoDecode(size_t input_size, const void* input, size_t* out_dimension, 
 	size_t compressed_data_size = 0;
 
 	int16_t* buffer_a = NULL;
-	int16_t* buffer_b = NULL;
+	uint8_t* buffer_b = NULL;
 
 	assert(input_size >= sizeof(struct AkoHead));
 	sReadHead(input, &dimension, &channels, &compressed_data_size);
 
 	data_len = dimension * dimension * channels;
 	buffer_a = calloc(1, sizeof(int16_t) * data_len);
-	buffer_b = calloc(1, sizeof(int16_t) * data_len);
+	buffer_b = calloc(1, sizeof(uint8_t) * data_len);
 	assert(buffer_a != NULL);
 	assert(buffer_b != NULL);
 
@@ -263,16 +258,16 @@ uint8_t* AkoDecode(size_t input_size, const void* input, size_t* out_dimension, 
 	DevBenchmarkStart("Unlift");
 	switch (channels)
 	{
-	case 4: sUnliftPlane(dimension, buffer_b, buffer_a + (dimension * dimension * 3));
-	case 3: sUnliftPlane(dimension, buffer_b, buffer_a + (dimension * dimension * 2));
-	case 2: sUnliftPlane(dimension, buffer_b, buffer_a + (dimension * dimension * 1));
-	case 1: sUnliftPlane(dimension, buffer_b, buffer_a); break;
+	case 4: sUnliftPlane(dimension, (void**)&buffer_b, buffer_a + (dimension * dimension * 3));
+	case 3: sUnliftPlane(dimension, (void**)&buffer_b, buffer_a + (dimension * dimension * 2));
+	case 2: sUnliftPlane(dimension, (void**)&buffer_b, buffer_a + (dimension * dimension * 1));
+	case 1: sUnliftPlane(dimension, (void**)&buffer_b, buffer_a); break;
 	}
 	DevBenchmarkStop();
 #endif
 
 	// DeTransform color-format
-	sDeTransformColorFormat(dimension, channels, buffer_a, (uint8_t*)buffer_b); // HACK!
+	sDeTransformColorFormat(dimension, channels, buffer_a, buffer_b); // HACK!
 
 	// Bye!
 	DevBenchmarkTotal();
@@ -283,5 +278,5 @@ uint8_t* AkoDecode(size_t input_size, const void* input, size_t* out_dimension, 
 		*out_channels = channels;
 
 	free(buffer_a);
-	return (uint8_t*)buffer_b; // TODO, realloc buffer
+	return (uint8_t*)buffer_b;
 }
