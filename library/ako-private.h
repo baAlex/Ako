73974ceddf7dfb63fec00dc3c61bf3433a742fb8
Alexander Brandt <brandt.alxndr@outlook.com>
2022-01-24 03:24:16 -0300

    Horizontal Cdf53 transform

--- library/ako-private.h
+++ library/ako-private.h
@@ -94,11 +94,11 @@ size_t akoImageMaxPlanesSpacingSize(size_t image_w, size_t image_h, size_t tiles
 
 // wavelet-cdf53.c:
 
-void akoCdf53LiftH(enum akoWrap, size_t current_h, size_t target_w, size_t fake_last, size_t in_stride,
+void akoCdf53LiftH(enum akoWrap, int16_t q, size_t current_h, size_t target_w, size_t fake_last, size_t in_stride,
                    const int16_t* in, int16_t* out);
 void akoCdf53LiftV(enum akoWrap, size_t target_w, size_t current_h, const int16_t* in, int16_t* out);
 
-void akoCdf53UnliftH(enum akoWrap, size_t current_w, size_t current_h, size_t out_stride, size_t ignore_last,
+void akoCdf53UnliftH(enum akoWrap, int16_t q, size_t current_w, size_t current_h, size_t out_stride, size_t ignore_last,
                      const int16_t* in_lp, const int16_t* in_hp, int16_t* out);
 void akoCdf53InPlaceishUnliftV(enum akoWrap, size_t current_w, size_t current_h, const int16_t* in_lp,
                                const int16_t* in_hp, int16_t* out_lp, int16_t* out_hp);
